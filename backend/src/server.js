c48ad015c4cdc8600d50cd1f053e4e18f0315377
Harsh Malviya <harshmalviya7415@gmail.com>
2026-08-23 15:26:02 +0530

    Allow any origin dynamically in CORS middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,12 +17,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:5174"
-  ],
+  origin: true,
   credentials: true
 }));
 app.use(express.json());
