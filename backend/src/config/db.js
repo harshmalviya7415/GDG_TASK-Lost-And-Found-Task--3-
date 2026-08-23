66bb5e21baa97330661c786b5ceec3afae188204
Harsh Malviya <harshmalviya7415@gmail.com>
2026-08-23 05:48:36 +0530

    mongo

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,9 +2,15 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
+let isConnected = false;
+
 const connectDb = async () => {
+    if (isConnected) {
+        return;
+    }
     try {
         const connect = await mongoose.connect(process.env.MONGO_URI);
+        isConnected = !!connect.connections[0].readyState;
         console.log("Server Connected:::::");
     } catch (error) {
         console.log("MONGO_URI:", process.env.MONGO_URI);
